c353e23d25eab599eb2ab8a1b6c9c02e0b62ca98
terry2870 <terry2870@163.com>
2016-10-20 23:07:37 +0800

    add

---
+++ mvc/src/main/java/org/mvc/App.java
@@ -0,0 +1,13 @@
+package org.mvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
